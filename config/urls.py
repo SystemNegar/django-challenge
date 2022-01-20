54d5440f008bb570aa2930443015ec9b9f48006d
habibi1981 <jaafhab@gmail.com>
2022-01-20 17:03:02 +0330

    feat: added swagger url in project

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path("doc/", include("swagger.urls")),
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls", namespace="accounts")),
 ]
