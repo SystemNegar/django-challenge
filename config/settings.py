cf7058b6c02796b8880f9efeb23896fa727eb4cb
habibi1981 <jaafhab@gmail.com>
2022-01-20 17:00:31 +0330

    feat: installed drf yasg

--- config/settings.py
+++ config/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "drf_yasg",
+    "swagger",
     "accounts.apps.AccountsConfig",
     "commons.apps.CommonsConfig",
 ]
