b1a1ee4b8cff5d0a31e1d498214da7afe4334474
habibi1981 <jaafhab@gmail.com>
2022-01-21 23:34:25 +0330

    refactor: refactor stadium app

--- arman/stadium/models.py
+++ arman/stadium/models.py
@@ -2,15 +2,18 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 from model_utils.models import TimeStampedModel
 
+from arman.city.models import City
+
 
 class Stadium(TimeStampedModel):
     name = models.CharField(max_length=256, blank=False, null=False)
     address = models.CharField(max_length=1024, blank=False, null=False)
-    city = models.ForeignKey(
-        "city.City", on_delete=models.CASCADE, related_name="stadiums"
-    )
+    city = models.ForeignKey(City, on_delete=models.CASCADE, related_name="stadiums")
 
     class Meta:
         db_table = "stadiums"
         verbose_name = _("stadium")
         verbose_name_plural = _("Stadium")
+
+    def __str__(self):
+        return self.name
--- arman/stadium/serializers.py
+++ arman/stadium/serializers.py
@@ -4,4 +4,4 @@ from rest_framework import serializers
 class StadiumCreateInputSerializer(serializers.Serializer):
     name = serializers.CharField(required=True)
     address = serializers.CharField(required=True)
-    city = serializers.IntegerField(required=True)
+    city_id = serializers.IntegerField(required=True)
