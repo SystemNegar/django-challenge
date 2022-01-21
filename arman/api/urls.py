70c43240b6a91ef7904af6c7002bb08909b61cbf
habibi1981 <jaafhab@gmail.com>
2022-01-21 21:49:24 +0330

    refacor: code refactor

--- arman/api/urls.py
+++ arman/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import include, path
 
 app_name = "api"
 urlpatterns = [
-    path("", include("arman.accounts.urls", namespace="accounts")),
-    path("", include("arman.stadium.urls", namespace="stadiums")),
+    path("accounts/", include("arman.accounts.urls", namespace="accounts")),
+    path("stadiums/", include("arman.stadium.urls", namespace="stadiums")),
     path("doc/", include("arman.swagger.urls")),
 ]
