53b071ebeca29facbb1083871b061d5b2a35fe03
habibi1981 <jaafhab@gmail.com>
2022-01-21 11:12:29 +0330

    feat: create otp api

--- arman/accounts/apis.py
+++ arman/accounts/apis.py
@@ -6,27 +6,50 @@ from rest_framework.views import APIView
 from arman.api.mixins import ApiErrorsMixin, ApiResponseDetailMixin
 from arman.api.schemas import response_schema
 
-from .serializers import SignupInputSerializer
-from .services import user_signup
+from .serializers import OTPInputSerializer, SignupInputSerializer
+from .services import register
 
 
-class SignupApi(ApiErrorsMixin, APIView):
+class SignUpApi(ApiErrorsMixin, APIView):
     permission_classes = [AllowAny]
 
     @swagger_auto_schema(
         request_body=SignupInputSerializer,
         security=[],
-        operation_description="User SignUp",
+        operation_description="SignUp",
         responses=response_schema,
     )
     def post(self, request):
         serializer = SignupInputSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
-        token = user_signup(phone=str(serializer.validated_data["phone"]))
+        token = register(phone=str(serializer.validated_data["phone"]))
 
         return ApiResponseDetailMixin(
             message="Account registered successfully",
+            status=status.HTTP_200_OK,
+            data={"token": token},
+            status_message="successful",
+        )
+
+
+class OTPApi(ApiErrorsMixin, APIView):
+    permission_classes = [AllowAny]
+
+    @swagger_auto_schema(
+        request_body=OTPInputSerializer,
+        security=[],
+        operation_description="OTP",
+        responses=response_schema,
+    )
+    def post(self, request):
+        serializer = OTPInputSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        token = register(phone=str(serializer.validated_data["phone"]))
+
+        return ApiResponseDetailMixin(
+            message="OTP registered successfully",
             status=status.HTTP_200_OK,
             data={"token": token},
             status_message="successful",
