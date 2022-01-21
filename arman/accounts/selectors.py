12ea8fd3f9b4a31c9d6f85ffaddcaf13c70b30eb
habibi1981 <jaafhab@gmail.com>
2022-01-21 15:01:40 +0330

    feat: create select user function

--- arman/accounts/selectors.py
+++ arman/accounts/selectors.py
@@ -1,5 +1,9 @@
-from .models import OTP
+from .models import OTP, CustomUser
 
 
 def get_otp_with_phone(phone):
     return OTP.objects.filter(phone__iexact=phone)
+
+
+def get_user_with_phone(phone):
+    return CustomUser.objects.filter(phone__iexact=phone)
