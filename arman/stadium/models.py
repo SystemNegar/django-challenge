3b0ba8e6071406375c03c14afd094f4d922117d2
habibi1981 <jaafhab@gmail.com>
2022-01-21 18:52:33 +0330

    feat: create stadium and city model

--- arman/stadium/models.py
+++ arman/stadium/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from model_utils.models import TimeStampedModel
 
-# Create your models here.
+
+class City(TimeStampedModel):
+    name = models.CharField(max_length=256, blank=False, null=False)
+
+    class Meta:
+        db_table = "cities"
+        verbose_name = _("city")
+        verbose_name_plural = _("City")
+
+
+class Stadium(TimeStampedModel):
+    name = models.CharField(max_length=256, blank=False, null=False)
+    address = models.CharField(max_length=1024, blank=False, null=False)
+    city = models.ForeignKey(City, on_delete=models.CASCADE, related_name="stadiums")
+
+    class Meta:
+        db_table = "stadiums"
+        verbose_name = _("stadium")
+        verbose_name_plural = _("Stadium")
