57cc47de2c5558a256cd33a7396652dda4886df9
habibi1981 <jaafhab@gmail.com>
2022-01-21 14:43:22 +0330

    refactor: changed model

--- arman/accounts/models.py
+++ arman/accounts/models.py
@@ -1,22 +1,21 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
-from model_utils.models import TimeStampedModel, UUIDModel
+from model_utils.models import TimeStampedModel
 from phonenumber_field.modelfields import PhoneNumberField
 
 
-class CustomUser(AbstractUser, UUIDModel):
+class CustomUser(AbstractUser):
     phone = PhoneNumberField(_("Phone"), unique=True, null=True, blank=True)
+
+    def __str__(self):
+        return str(self.phone)
 
 
 class OTP(TimeStampedModel):
     phone = PhoneNumberField(_("Phone"), unique=True, null=True, blank=True)
     otp = models.CharField(max_length=9, blank=True, null=True)
     register_flag = models.BooleanField(default=False)
-    expired_date = models.DateTimeField(
-        default=timezone.now() + timezone.timedelta(minutes=1)
-    )
 
     def __str__(self):
         return str(self.otp) + " sent to " + str(self.phone)
