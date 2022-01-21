8f74f9b5fa0867da3b3e23e6602406b0df6414af
habibi1981 <jaafhab@gmail.com>
2022-01-21 23:35:47 +0330

    feat: added city path in api app urls

--- arman/api/urls.py
+++ arman/api/urls.py
@@ -4,5 +4,6 @@ app_name = "api"
 urlpatterns = [
     path("accounts/", include("arman.accounts.urls", namespace="accounts")),
     path("stadiums/", include("arman.stadium.urls", namespace="stadiums")),
+    path("cities/", include("arman.city.urls", namespace="cities")),
     path("doc/", include("arman.swagger.urls")),
 ]
