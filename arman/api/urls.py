338afe55629e169c09ff244065b14b6a4668a859
habibi1981 <jaafhab@gmail.com>
2022-01-21 18:36:58 +0330

    feat: added stadium path in api urls

--- arman/api/urls.py
+++ arman/api/urls.py
@@ -3,5 +3,6 @@ from django.urls import include, path
 app_name = "api"
 urlpatterns = [
     path("", include("arman.accounts.urls", namespace="accounts")),
+    path("", include("arman.stadium.urls", namespace="stadiums")),
     path("doc/", include("arman.swagger.urls")),
 ]
