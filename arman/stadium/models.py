b171efce0e33a098d521f7cd70e4e6043f33e128
habibi1981 <jaafhab@gmail.com>
2022-01-21 22:41:32 +0330

    reafctor: remove city model

--- arman/stadium/models.py
+++ arman/stadium/models.py
@@ -3,19 +3,12 @@ from django.utils.translation import gettext_lazy as _
 from model_utils.models import TimeStampedModel
 
 
-class City(TimeStampedModel):
-    name = models.CharField(max_length=256, blank=False, null=False)
-
-    class Meta:
-        db_table = "cities"
-        verbose_name = _("city")
-        verbose_name_plural = _("City")
-
-
 class Stadium(TimeStampedModel):
     name = models.CharField(max_length=256, blank=False, null=False)
     address = models.CharField(max_length=1024, blank=False, null=False)
-    city = models.ForeignKey(City, on_delete=models.CASCADE, related_name="stadiums")
+    city = models.ForeignKey(
+        "city.City", on_delete=models.CASCADE, related_name="stadiums"
+    )
 
     class Meta:
         db_table = "stadiums"
