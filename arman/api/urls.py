decc02ec6278e5bc648b1c637738e12dd4a9f142
habibi1981 <jaafhab@gmail.com>
2022-01-21 16:31:59 +0330

    refactor: code refactor

--- arman/api/urls.py
+++ arman/api/urls.py
@@ -2,6 +2,6 @@ from django.urls import include, path
 
 app_name = "api"
 urlpatterns = [
-    path("accounts/", include("arman.accounts.urls", namespace="accounts")),
+    path("", include("arman.accounts.urls", namespace="accounts")),
     path("doc/", include("arman.swagger.urls")),
 ]
