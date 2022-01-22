a360a3fc0b288fd3508a55dd1adf7687df95f7f4
habibi1981 <jaafhab@gmail.com>
2022-01-22 21:04:47 +0330

    refactor: code refactor

--- arman/stadium/services.py
+++ arman/stadium/services.py
@@ -2,7 +2,7 @@ from .models import Stadium
 
 
 def create_stadium(**validate_data):
-    # try:
-    Stadium.objects.create(**validate_data)
-    # except Exception:
-    #     raise ValueError("can't create stadium with given fields")
+    try:
+        Stadium.objects.create(**validate_data)
+    except Exception:
+        raise ValueError("can't create stadium with given fields")
