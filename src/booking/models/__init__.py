11f9360ed601ea7a0cdfb0216c18850a0a59619b
Mahdi Namaki <mavenium@gmail.com>
2023-10-26 21:14:17 +0330

    Add: The 'Match' model

--- src/booking/models/__init__.py
+++ src/booking/models/__init__.py
@@ -1,9 +1,11 @@
 from .stadium import Stadium
 from .section import Section
 from .team import Team
+from .match import Match
 
 __all__ = [
     'Stadium',
     'Section',
     'Team',
+    'Match',
 ]
