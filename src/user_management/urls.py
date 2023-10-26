df1bbd02c6d8896995434f21cccb2cf3643fbda2
Mahdi Namaki <mavenium@gmail.com>
2023-10-26 13:11:06 +0330

    Fix: Add the 'users' paths

--- src/user_management/urls.py
+++ src/user_management/urls.py
@@ -5,5 +5,6 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 router.register('permissions', views.PermissionViewSet, basename='permissions')
 router.register('groups', views.GroupViewSet, basename='groups')
+router.register('users', views.UserViewSet, basename='users')
 
 urlpatterns = router.urls
