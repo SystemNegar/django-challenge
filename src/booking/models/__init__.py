dbc33b9e086acede99700e83c861b7b5d519bca9
Mahdi Namaki <mavenium@gmail.com>
2023-10-26 22:26:51 +0330

    Add: The 'Invoice' model

--- src/booking/models/__init__.py
+++ src/booking/models/__init__.py
@@ -2,10 +2,12 @@ from .stadium import Stadium
 from .section import Section
 from .team import Team
 from .match import Match
+from .invoice import Invoice
 
 __all__ = [
     'Stadium',
     'Section',
     'Team',
     'Match',
+    'Invoice',
 ]
---
+++ src/booking/models/invoice.py
@@ -0,0 +1,30 @@
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth import get_user_model
+from django.db import models
+
+from extensions.abstract_models import AbstractCreatAtUpdateAt
+from extensions.choices import InvoiceStatusChoices
+
+
+class Invoice(AbstractCreatAtUpdateAt, models.Model):
+    user = models.ForeignKey(
+        get_user_model(),
+        verbose_name=_('User'),
+        on_delete=models.CASCADE,
+        related_name='invoice_users'
+    )
+    status = models.PositiveSmallIntegerField(
+        verbose_name=_('Status'),
+        choices=InvoiceStatusChoices.choices,
+        default=InvoiceStatusChoices.UNPAID
+    )
+
+    class Meta:
+        app_label = 'booking'
+        db_table = 'booking_invoices'
+        verbose_name = _('Invoice')
+        verbose_name_plural = _('Invoices')
+        ordering = ['id']
+
+    def __str__(self) -> str:
+        return f"{self.user} - {self.id}"
