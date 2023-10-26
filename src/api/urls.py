24fed07553577ba81f638670957d1cf0d70a697b
Mahdi Namaki <mavenium@gmail.com>
2023-10-26 10:49:00 +0330

    Fix: Add the 'swagger' & 'redoc' & 'token' & 'user_management' paths

---
+++ src/api/urls.py
@@ -0,0 +1,33 @@
+from django.urls import path, include
+
+from rest_framework import permissions
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+from extensions.custom_permissions import CustomDjangoModelPermission
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Ticketing System API",
+        default_version='v1',
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="mavenium@gmail.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.DjangoModelPermissions, CustomDjangoModelPermission),
+    authentication_classes=(JWTAuthentication,)
+)
+
+urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('', include(('user_management.urls', 'user_management'), namespace='user_management')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema_swagger_ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema_redoc'),
+]
