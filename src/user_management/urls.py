9f689b796c5037d286ca5abc06024419e8f37437
Mahdi Namaki <mavenium@gmail.com>
2023-10-26 10:58:06 +0330

    Fix: Add the 'router' & 'permissions' paths

--- src/user_management/urls.py
+++ src/user_management/urls.py
@@ -1,1 +1,8 @@
-urlpatterns = []
+from . import views
+
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('permissions', views.PermissionViewSet, basename='permissions')
+
+urlpatterns = router.urls
