642bf3fa2cf31b1b6154be7e9b485a0a0252c0e1
Mahdi Namaki <mavenium@gmail.com>
2023-10-26 09:16:31 +0330

    Add: The 'commands' app

--- src/core/settings.py
+++ src/core/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'user_management.apps.UserManagementConfig',
+    'commands.apps.CommandsConfig',
 ]
 
 MIDDLEWARE = [
