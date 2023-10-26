39e0fd26eded3d2a4ad7d17acb10a186f59041fb
Mahdi Namaki <mavenium@gmail.com>
2023-10-26 11:30:38 +0330

    Fix: Add the 'groups' paths

--- src/user_management/urls.py
+++ src/user_management/urls.py
@@ -4,5 +4,6 @@ from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register('permissions', views.PermissionViewSet, basename='permissions')
+router.register('groups', views.GroupViewSet, basename='groups')
 
 urlpatterns = router.urls
