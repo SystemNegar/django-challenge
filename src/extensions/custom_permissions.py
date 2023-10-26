73422b36ce6a213b4c9e5530a9db4ee5e5776901
Mahdi Namaki <mavenium@gmail.com>
2023-10-26 16:16:16 +0330

    Fix: Add docstring

--- src/extensions/custom_permissions.py
+++ src/extensions/custom_permissions.py
@@ -1,16 +1,17 @@
-from rest_framework.permissions import DjangoModelPermissions, BasePermission, SAFE_METHODS
+from rest_framework.permissions import DjangoModelPermissions, BasePermission
 
 from copy import deepcopy
 
 
 class CustomDjangoModelPermission(DjangoModelPermissions):
-
+    """Custom permission class to handle better permission by model permissions"""
     def __init__(self):
         self.perms_map = deepcopy(self.perms_map)
         self.perms_map['GET'] = ['%(app_label)s.view_%(model_name)s']
 
 
 class UnauthenticatedPost(BasePermission):
+    """Allow to unauthenticated user to send the 'POST' request"""
     def has_permission(self, request, view):
         return request.method in ['POST']
 
