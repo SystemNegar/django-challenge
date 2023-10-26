70718d2bf0e37f166fe024c90eaf7fce00d31077
Mahdi Namaki <mavenium@gmail.com>
2023-10-26 12:31:54 +0330

    Fix: Add the 'UnauthenticatedPost' class

--- src/extensions/custom_permissions.py
+++ src/extensions/custom_permissions.py
@@ -1,4 +1,4 @@
-from rest_framework.permissions import DjangoModelPermissions
+from rest_framework.permissions import DjangoModelPermissions, BasePermission
 
 from copy import deepcopy
 
@@ -8,3 +8,8 @@ class CustomDjangoModelPermission(DjangoModelPermissions):
     def __init__(self):
         self.perms_map = deepcopy(self.perms_map)
         self.perms_map['GET'] = ['%(app_label)s.view_%(model_name)s']
+
+
+class UnauthenticatedPost(BasePermission):
+    def has_permission(self, request, view):
+        return request.method in ['POST']
