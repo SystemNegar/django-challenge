460f44a30a58b73fbe2d19059fd4bcbe29f2b425
Mahdi Namaki <mavenium@gmail.com>
2023-10-27 10:23:44 +0330

    Add: The 'stadiums' paths

--- src/booking/urls.py
+++ src/booking/urls.py
@@ -1,1 +1,8 @@
-urlpatterns = []
+from . import views
+
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('stadiums', views.StadiumViewSet, basename='stadiums')
+
+urlpatterns = router.urls
