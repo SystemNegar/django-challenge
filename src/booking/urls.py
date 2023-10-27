168b2e1b751be685845055b2774541c9f3555773
Mahdi Namaki <mavenium@gmail.com>
2023-10-27 11:51:03 +0330

    Add: The 'invoices' paths

--- src/booking/urls.py
+++ src/booking/urls.py
@@ -7,5 +7,6 @@ router.register('stadiums', views.StadiumViewSet, basename='stadiums')
 router.register('sections', views.SectionViewSet, basename='sections')
 router.register('teams', views.TeamViewSet, basename='teams')
 router.register('matches', views.MatchViewSet, basename='matches')
+router.register('invoices', views.InvoiceViewSet, basename='invoices')
 
 urlpatterns = router.urls
