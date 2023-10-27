75e65a9dcefdef17fc0afe45c658f5916834fa20
Mahdi Namaki <mavenium@gmail.com>
2023-10-27 10:48:04 +0330

    Add: The 'TeamViewSet' class

--- src/booking/views/__init__.py
+++ src/booking/views/__init__.py
@@ -1,7 +1,9 @@
 from .stadium import StadiumViewSet
 from .section import SectionViewSet
+from .team import TeamViewSet
 
 __all__ = [
     'StadiumViewSet',
     'SectionViewSet',
+    'TeamViewSet',
 ]
