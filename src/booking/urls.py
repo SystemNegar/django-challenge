57a9efe7c3cd46834bb005db56f3e9c9feb617b6
Mahdi Namaki <mavenium@gmail.com>
2023-10-27 11:11:12 +0330

    Add: The 'matches' paths

--- src/booking/urls.py
+++ src/booking/urls.py
@@ -6,5 +6,6 @@ router = DefaultRouter()
 router.register('stadiums', views.StadiumViewSet, basename='stadiums')
 router.register('sections', views.SectionViewSet, basename='sections')
 router.register('teams', views.TeamViewSet, basename='teams')
+router.register('matches', views.MatchViewSet, basename='matches')
 
 urlpatterns = router.urls
