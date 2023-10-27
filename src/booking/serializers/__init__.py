7d4040960085395a574288f7867d1fb7a2686927
Mahdi Namaki <mavenium@gmail.com>
2023-10-27 11:45:28 +0330

    Add: The 'InvoiceSerializer' class

--- src/booking/serializers/__init__.py
+++ src/booking/serializers/__init__.py
@@ -2,10 +2,12 @@ from .stadium import StadiumSerializer
 from .section import SectionSerializer
 from .team import TeamSerializer
 from .match import MatchSerializer
+from .invoice import InvoiceSerializer
 
 __all__ = [
     'StadiumSerializer',
     'SectionSerializer',
     'TeamSerializer',
     'MatchSerializer',
+    'InvoiceSerializer',
 ]
---
+++ src/booking/serializers/invoice.py
@@ -0,0 +1,19 @@
+from rest_framework.serializers import ModelSerializer
+
+from booking.models import Invoice
+
+
+class InvoiceSerializer(ModelSerializer):
+    """
+    Serializer for the 'Invoice' model
+    """
+
+    class Meta:
+        model = Invoice
+        fields = '__all__'
+        read_only_fields = ('created_at', 'updated_at')
+
+    def to_representation(self, obj):
+        data = super(InvoiceSerializer, self).to_representation(obj)
+        data['status'] = obj.get_status_display()
+        return data
