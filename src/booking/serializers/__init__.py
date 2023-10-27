24ffcff2f30e972d8088b0a0955e8169f8ca7da5
Mahdi Namaki <mavenium@gmail.com>
2023-10-27 10:46:56 +0330

    Add: The 'TeamSerializer' class

--- src/booking/serializers/__init__.py
+++ src/booking/serializers/__init__.py
@@ -1,7 +1,9 @@
 from .stadium import StadiumSerializer
 from .section import SectionSerializer
+from .team import TeamSerializer
 
 __all__ = [
     'StadiumSerializer',
     'SectionSerializer',
+    'TeamSerializer',
 ]
