ac90e9d81df9f8f10922f23d56dd0a15aef26b34
Mahdi Namaki <mavenium@gmail.com>
2023-10-27 09:17:16 +0330

    Fix: Add the 'SectionInlineAdmin' & 'StadiumAdmin' classes

--- src/booking/admin.py
+++ src/booking/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
 
-# Register your models here.
+from booking.models import Stadium, Section
+
+
+class SectionInlineAdmin(admin.TabularInline):
+    model = Section
+    min_num = 1
+    extra = 0
+
+
+class StadiumAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'province',
+        'city',
+        'created_at',
+        'updated_at',
+    ]
+
+    list_filter = [
+        'province',
+        'city',
+        'created_at',
+        'updated_at',
+    ]
+
+    search_fields = [
+        'name',
+        'province',
+        'city',
+    ]
+
+    actions = [
+        'delete_selected',
+    ]
+
+    inlines = (
+        SectionInlineAdmin,
+    )
+
+    def get_queryset(self, request):
+        return self.model.objects.prefetch_related('section_stadiums')
+
+
+admin.site.register(Stadium, StadiumAdmin)
