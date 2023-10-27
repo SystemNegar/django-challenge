b64c1f11b8aa4001b48a829a1db2f8fe69cef6c8
Mahdi Namaki <mavenium@gmail.com>
2023-10-27 10:34:50 +0330

    Add: The 'sections' paths

--- src/booking/urls.py
+++ src/booking/urls.py
@@ -4,5 +4,6 @@ from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register('stadiums', views.StadiumViewSet, basename='stadiums')
+router.register('sections', views.SectionViewSet, basename='sections')
 
 urlpatterns = router.urls
