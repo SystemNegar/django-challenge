bce7496bc32f60c31f14b7fef93db7cd25cedc35
Mahdi Namaki <mavenium@gmail.com>
2023-10-27 09:24:54 +0330

    Fix: Add the 'TeamAdmin' class

--- src/booking/admin.py
+++ src/booking/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from booking.models import Stadium, Section
+from booking.models import Stadium, Section, Team
 
 
 class SectionInlineAdmin(admin.TabularInline):
@@ -43,4 +43,26 @@ class StadiumAdmin(admin.ModelAdmin):
         return self.model.objects.prefetch_related('section_stadiums')
 
 
+class TeamAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'created_at',
+        'updated_at',
+    ]
+
+    list_filter = [
+        'created_at',
+        'updated_at',
+    ]
+
+    search_fields = [
+        'name',
+    ]
+
+    actions = [
+        'delete_selected',
+    ]
+
+
 admin.site.register(Stadium, StadiumAdmin)
+admin.site.register(Team, TeamAdmin)
