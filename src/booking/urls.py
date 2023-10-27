ef33c43152f1de86ac49d33564cf166327c7129a
Mahdi Namaki <mavenium@gmail.com>
2023-10-27 10:48:24 +0330

    Add: The 'teams' paths

--- src/booking/urls.py
+++ src/booking/urls.py
@@ -5,5 +5,6 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 router.register('stadiums', views.StadiumViewSet, basename='stadiums')
 router.register('sections', views.SectionViewSet, basename='sections')
+router.register('teams', views.TeamViewSet, basename='teams')
 
 urlpatterns = router.urls
